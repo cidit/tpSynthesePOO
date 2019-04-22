8b1f8c5747f3eb1b2093c55d6f080792a217a197
cid <ciditworld@gmail.com>
2019-04-22 13:30:08 -0400

    added the base for the missile

---
+++ src/assets/entities/Missile.java
@@ -0,0 +1,22 @@
+package assets.entities;
+
+import assets.graphic.Animation;
+import assets.util.Coordinates;
+
+public class Missile extends Projectile {
+
+	public Missile(Coordinates position, Animation composition) {
+		super(position, composition);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//add a ENUM getter for the currentState (flying, hitting, destroyed)
+	
+
+}
