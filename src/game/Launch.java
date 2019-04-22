bff1e8fbc86901794e16d74d20aed0e7e7391e35
cidit <ciditworld@gmail.com>
2019-04-21 20:19:00 -0400

    main

---
+++ src/game/Launch.java
@@ -0,0 +1,19 @@
+package game;
+
+import javax.swing.SwingUtilities;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+	}
+
+}
