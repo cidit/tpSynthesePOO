f9e9a7b46b406a7e27e9942f0fba5ebe9560c07f
cid <ciditworld@gmail.com>
2019-04-22 12:16:07 -0400

    added a comment

--- src/assets/entities/Projectile.java
+++ src/assets/entities/Projectile.java
@@ -8,5 +8,7 @@ public abstract class Projectile extends Entity implements Automaton {
 	public Projectile(Coordinates position, Animation composition) {
 		super(position, composition);
 	}
+	
+	//may be able to use ENUMs for the type of projectile and the power
 
 }
