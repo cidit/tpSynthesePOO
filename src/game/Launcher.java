f38f5ceaa71d94be099cd63f01b5a9bdcecfa450
cidit <ciditworld@gmail.com>
2019-04-26 15:15:04 -0400

    added tidbit

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -4,20 +4,25 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Launcher {
+	private static JFrame frame;
+	private static Scoreboard sb;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			
 			@Override
 			public void run() {
-				JFrame frame = new JFrame("Space Invader 0.1");
-				frame.setSize(512, 512);
-				frame.setVisible(true);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				initJFrame();
 				
 			}
 		});
-
+	}
+	
+	public static void initJFrame() {
+		frame = new JFrame("Space Invader 0.1");
+		frame.setSize(512, 512);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
