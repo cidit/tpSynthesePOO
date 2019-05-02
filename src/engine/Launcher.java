6846940883cfe41b8cc1c6ae46646d36e2137d6c
cid <ciditworld@gmail.com>
2019-05-02 15:06:37 -0400

    made the JFrame unresizable

--- src/engine/Launcher.java
+++ src/engine/Launcher.java
@@ -21,6 +21,7 @@ public class Launcher {
 	public static void initJFrame() {
 		frame = new JFrame("Space Invader 0.1");
 		frame.setSize(512, 512);
+		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
