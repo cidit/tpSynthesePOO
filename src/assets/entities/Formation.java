b4eed342626c9fb8073b317c406dfaf92363b663
cid <ciditworld@gmail.com>
2019-05-07 10:59:00 -0400

    d6fygu

--- src/assets/entities/Formation.java
+++ src/assets/entities/Formation.java
@@ -13,12 +13,23 @@ public final class Formation{
 		this.rows = rows;
 		this.columns = columns;
 		array = new ArrayList<Column>(columns);
+		fill();
 	}
 	
+	private void fill() {
+		for (int i = 0; i < columns; i++) {
+			Column c = new Column(rows);
+		}
+	}
+
 	private static class Column extends ArrayList<Invader> {
 		
-		public Invader getLast() {
-			return get(size());
+		public Column(int capacity) {
+			super(capacity);
+		}
+		
+		public Invader last() {
+			return get(size()-1);
 		}
 		
 	}
