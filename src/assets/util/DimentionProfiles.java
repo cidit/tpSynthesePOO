71f00cf1b4633187f17686e11f6048577fc638b3
cid <ciditworld@gmail.com>
2019-05-09 13:09:38 -0400

    added functionality to DimentionProfiles

--- src/assets/util/DimentionProfiles.java
+++ src/assets/util/DimentionProfiles.java
@@ -5,24 +5,41 @@ public class DimentionProfiles {
 	private static Dimention canonDimention;
 	private static Dimention invaderDimention;
 	private static Dimention missileDimention;
+	private static boolean initialised = false;
 
 	public static void setProfiles(Dimention canon, Dimention invader, Dimention missile) {
 		canonDimention = canon;
 		invaderDimention = invader;
 		missileDimention = missile;
+		initialised = true;
 	}
 	
 	public static Dimention getCanon() {
+		if (!initialised) {
+			System.out.println("DIMENTION PROFILES UNINITIALISED");
+			return null;
+		}
 		return canonDimention;
 	}
 
 	public static Dimention getInvader() {
+		if (!initialised) {
+			System.out.println("DIMENTION PROFILES UNINITIALISED");
+			return null;
+		}
 		return invaderDimention;
 	}
 
 	public static Dimention getMissile() {
+		if (!initialised) {
+			System.out.println("DIMENTION PROFILES UNINITIALISED");
+			return null;
+		}
 		return missileDimention;
 	}
 	
+	public static boolean isInitialised() {
+		return initialised;
+	}
 
 }
