37be28648587fc0fee2546db2a5ab8fe9071bc76
cid <ciditworld@gmail.com>
2019-05-09 13:24:51 -0400

    should be done with Canon

--- src/assets/entities/Canon.java
+++ src/assets/entities/Canon.java
@@ -5,28 +5,36 @@ import assets.entities.enumerations.Direction;
 import assets.entities.interfaces.Fireable;
 import assets.util.Coordinate;
 import assets.util.Counter;
+import assets.util.DimentionProfiles;
+import assets.util.Hitbox;
 
 public final class Canon extends Entity implements Fireable {
 	
 	private Counter fireInterval;
 
-	public Canon(Coordinate position) {
-		super(position, Allegiance.FRIENDLY);
+	public Canon(Coordinate coordinate) {
+		super(new Hitbox(coordinate, DimentionProfiles.getCanon()), Allegiance.FRIENDLY);
 		fireInterval = new Counter(10);
 	}
 
 	@Override
 	public Missile fire() {
 		if (fireInterval.isExpired()) {
-			Missile missile = new Missile(position, allegiance);
-			missile.setDirection(Direction.UP);
+			// center the missile properly
+			int x = hitbox.getDimention().getWidth() / 2 - DimentionProfiles.getMissile().getWidth() / 2;
+			int y = hitbox.getSides().getTop() - DimentionProfiles.getMissile().getHeight();
+			
+			Coordinate coordinate = new Coordinate(x, y);
+			Missile missile = new Missile(coordinate, allegiance);
+			missile.changeDirection(Direction.UP);
 			fireInterval.reset();
 			return missile;
 		}
 		return null;
 	}
 	
-	public void updateFireRateTimer() {
+	@Override
+	public void update() {
 		fireInterval.count();
 	}
 
