7ec2208930e1d53aed323a4e44026468d37bc835
cid <ciditworld@gmail.com>
2019-05-09 13:02:45 -0400

    should be finished with Invader

--- src/assets/entities/Invader.java
+++ src/assets/entities/Invader.java
@@ -6,22 +6,28 @@ import assets.entities.enumerations.Allegiance;
 import assets.entities.enumerations.Direction;
 import assets.entities.interfaces.Fireable;
 import assets.util.Coordinate;
+import assets.util.DimentionProfiles;
+import assets.util.Hitbox;
 
 public final class Invader extends Entity implements Fireable {
 
 	Random randomizer = new Random();
 	private static double FIRE_PROBABILITY = 1 / 150;
 
-	public Invader(Coordinate position) {
-		super(position, Allegiance.HOSTILE);
-		// TODO Auto-generated constructor stub
+	public Invader(Coordinate coordinate) {
+		super(new Hitbox(coordinate, DimentionProfiles.getInvader()), Allegiance.HOSTILE);
 	}
 
 	@Override
 	public Missile fire() {
-		if (randomizer.nextDouble() <= FIRE_PROBABILITY) {
-			Missile missile = new Missile(position, allegiance);
-			missile.setDirection(Direction.DOWN);
+		if (randomizer.nextDouble() < FIRE_PROBABILITY) {
+			// center the missile properly
+			int x = hitbox.getDimention().getWidth() / 2 - DimentionProfiles.getMissile().getWidth() / 2;
+			int y = hitbox.getSides().getBottom();
+			
+			Coordinate coordinate = new Coordinate(x, y);
+			Missile missile = new Missile(new Hitbox(coordinate, DimentionProfiles.getMissile()), allegiance);
+			missile.changeDirection(Direction.DOWN);
 			return missile;
 		}
 		return null;
